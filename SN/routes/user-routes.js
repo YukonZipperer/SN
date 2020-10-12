2dedf7df9959e356ac253ff56baf4c6a5bc20adf
YukonZipperer <70924175+YukonZipperer@users.noreply.github.com>
2020-10-12 13:03:06 -0500

    Added documentation and new routes
    I have added some documentation as well as new routes to access the newly created update and delete methods for the user profiles.

--- SN/routes/user-routes.js
+++ SN/routes/user-routes.js
@@ -2,15 +2,17 @@
 const express = require("express");
 //configures express router
 const router = express.Router();
-
+//requires the requireSignin function from authen.js
 const {requireSignin} = require("../ctrls/authen.js");
 
-
-const {findId, getUser, all} = require("../ctrls/user.js");
-
+//requires methods from the user.js controller
+const {findId, getUser, all, profileUpdate, deleteProfile} = require("../ctrls/user.js");
+//routes to access methods for user
 router.get("/users", all);
 router.get("/user/:userid", requireSignin, getUser);
-
+router.put("/user/:userid", requireSignin, profileUpdate);
+router.delete("/user/:userid", requireSignin, deleteProfile);
+//whenever the variable 'userid' is in the request, it will find the user
 router.param("userid", findId);
 
 
