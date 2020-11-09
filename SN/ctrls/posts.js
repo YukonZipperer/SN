b46594d6af80d996539d1ed0c35dc0ad9c8e505f
YukonZipperer <70924175+YukonZipperer@users.noreply.github.com>
2020-11-09 14:47:51 -0600

    Updated Post Controller
    I have added a method to get posts by a user's id. Posts are returned by date of creation. I also updated the create post method to allow for image upload.

--- SN/ctrls/posts.js
+++ SN/ctrls/posts.js
@@ -1,10 +1,13 @@
 //requires Post Schema
 const Post = require("../models/post.js");
-
+const formidable = require("formidable");
+const fileSystem = require("fs");
 //exports getposts function
 exports.getPosts = function(req, res){
     //finds post from database
-    const posts = Post.find().select("_id title body")
+    const posts = Post.find()
+    .populate("author", "_id name")
+    .select("_id title body")
         .then(posts => {
             //returns posts found in json
             res.json({ posts });
@@ -12,13 +15,44 @@ exports.getPosts = function(req, res){
 };
 //exports createpost function
 exports.createPost = function(req, res){
-    //creates new post and saves it as post variable
-    const post = new Post(req.body);
-    //saves post into database
-    post.save(function(err, savedPost){
-            res.json({
-                //sends post back in json
-                post: savedPost
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err){
+            console.log("FORM Parse error");
+            return res.status(400).json({
+                err: "Image could not be uploaded"
             })
+        }
+        let post = new Post(fields);
+        req.profile.hash_pass = undefined;
+        req.profile.salt = undefined;
+        post.author = req.profile;
+        if(files.photo){
+            post.photo.data = fileSystem.readFileSync(files.photo.path)
+            post.photo.contentType = files.photo.type
+        }
+        post.save(function(err, comp){
+            if(err){
+                return res.status(400).json({
+                    err: err
+                })
+            }
+            res.json(comp)
+        });
+    });
+};
+
+exports.postsById = function(req, res){
+    Post.find({author: req.profile._id})
+    .populate("author", "_id name")
+    .sort("_created")
+    .exec(function(err, posts){
+        if(err){
+            return res.status(400).json({
+                err: err
+            });
+        }
+        res.json(posts);
     });
 };
