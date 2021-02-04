7bf43e544d16c393be8733c59bd81529e06408f9
yZipperer <70924175+yZipperer@users.noreply.github.com>
2021-02-04 08:03:01 -0700

    Updated Methods
    Now you can upload a profile image

--- SN/ctrls/user.js
+++ SN/ctrls/user.js
@@ -2,6 +2,9 @@
 const _ = require("lodash");
 //requires User from user.js
 const User = require("../models/user.js");
+
+const fs = require("fs");
+const formidable = require("formidable");
 //method to find a user by id
 exports.findId = function(req, res, next, id){
     User.findById(id).exec(function(err, user){
@@ -14,13 +17,13 @@ exports.findId = function(req, res, next, id){
         next();
     });
 
-}
+};
 //method to get profile information about user
 exports.getUser = function(req, res){
     req.profile.hash_pass = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
-}
+};
 
 //checks to make sure you are authorized to view profile
 exports.isAuth = function(req, res, next){
@@ -30,24 +33,49 @@ exports.isAuth = function(req, res, next){
             err: "User is not authorized"
         });
     }
-}
+};
 //method to update profile
+
+
 exports.profileUpdate = function(req, res, next) {
-    let user = req.profile;
-    user = _.extend(user, req.body);
-    user.updated = Date.now();
-    user.save(function(err){
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, function(err, f, files){
         if(err){
             return res.status(400).json({
-                err: "You are not authorized"
+                err: "Photo upload failed"
             });
         }
+        let user = req.profile;
+        user = _.extend(user, f);
+        user.updated = Date.now();
 
-        user.hash_pass = undefined;
-        user.salt = undefined;
-        res.json({user});
-    });
-}
+        if(files.photo){
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+
+        user.save(function(err, result){
+            if(err){
+                return res.status(400).json({
+                    err: err
+                });
+            }
+
+            user.hash_pass = undefined;
+            user.salt = undefined;
+            res.json(user);
+        })
+    })
+};
+
+exports.getProfileImage = function(req, res, next){
+    if(req.profile.photo.data){
+        res.set(("Content-Type", req.profile.photo.contentType));
+        return res.send(req.profile.photo.data);
+    }
+    next();
+};
 //method to delete profile
 exports.deleteProfile = function(req, res, next){
     let user = req.profile;
@@ -61,7 +89,7 @@ exports.deleteProfile = function(req, res, next){
             msg: "Account successfully deleted"
         });
     });
-}
+};
 //method to find all profiles
 exports.all = function(req, res){
     User.find(function(err, userf){
@@ -72,5 +100,5 @@ exports.all = function(req, res){
         }
         res.json(userf)
     }).select("name email updated created");
-}
+};
 
