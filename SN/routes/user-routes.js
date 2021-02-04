10bcaf3a01e0ca6a4088afc66f1d49852a8b664c
yZipperer <70924175+yZipperer@users.noreply.github.com>
2021-02-04 08:03:48 -0700

    Added new route
    New route to get the profile image of a user

--- SN/routes/user-routes.js
+++ SN/routes/user-routes.js
@@ -6,12 +6,13 @@ const router = express.Router();
 const {requireSignin} = require("../ctrls/authen.js");
 
 //requires methods from the user.js controller
-const {findId, getUser, all, profileUpdate, deleteProfile} = require("../ctrls/user.js");
+const {findId, getUser, all, profileUpdate, deleteProfile, getProfileImage} = require("../ctrls/user.js");
 //routes to access methods for user
 router.get("/users", all);
 router.get("/user/:userid", requireSignin, getUser);
 router.put("/user/:userid", requireSignin, profileUpdate);
 router.delete("/user/:userid", requireSignin, deleteProfile);
+router.get("/user/photo/:userid", getProfileImage);
 //whenever the variable 'userid' is in the request, it will find the user
 router.param("userid", findId);
 
